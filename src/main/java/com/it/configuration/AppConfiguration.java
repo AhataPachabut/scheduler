1f69a77a3c9bf52362a2ca7e9e0ab98719459e66
AgataPochebut <pochebut.a@gmail.com>
2019-12-12 01:12:58 +0300

    13

--- src/main/java/com/it/configuration/AppConfiguration.java
+++ src/main/java/com/it/configuration/AppConfiguration.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
 @Configuration
-@ComponentScan("com.it")
+@ComponentScan(basePackages = "com.it")
 @Import({WebConfiguration.class, DatabaseConfiguration.class})
 public class AppConfiguration {
 
